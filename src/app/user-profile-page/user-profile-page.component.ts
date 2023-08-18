352e1d393b1e68b78527ee2e9a724c01a69213e5
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-08-18 14:39:57 -0600

    Create user-profile-page.component.ts

---
+++ src/app/user-profile-page/user-profile-page.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { UserEditDialogComponent } from '../user-edit-dialog/user-edit-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-user-profile-page',
+  templateUrl: './user-profile-page.component.html',
+  styleUrls: ['./user-profile-page.component.scss']
+})
+
+export class UserProfilePageComponent {
+  user: any = {};
+  favMovies: any = [{}];
+  
+  constructor(
+    private fetchApiData: FetchApiDataService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+  
+  getUser(): void {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}')
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.favMovies = resp.filter((m: {_id: any}) => this.user.favoriteMovies.indexOf(m._id) >= 0)
+    })
+    console.log('favMovies', this.favMovies)
+      return console.log(this.user)
+  }
+
+  openeditUserDialog(): void {
+    this.dialog.open(UserEditDialogComponent, {
+      // Assigning the dialog a width
+          width: '280px'
+          });      
+  }
+  
+}
+
+
